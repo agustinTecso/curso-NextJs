0889398aa249ebf99f52ab7b8da37271f377d239
agustin <agustin.albornoz@tecso.coop>
2024-07-01 17:47:44 -0300

    rollback cambios de conexion

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-/*import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
@@ -100,19 +100,19 @@ async function seedRevenue() {
 
   return insertedRevenue;
 }
-*/
+
 export async function GET() {
   try {
-/*    await client.sql`BEGIN`;
+    await client.sql`BEGIN`;
     await seedUsers();
     await seedCustomers();
     await seedInvoices();
     await seedRevenue();
     await client.sql`COMMIT`;
-*/
+
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
-/*    await client.sql`ROLLBACK`;*/
+    await client.sql`ROLLBACK`;
     return Response.json({ error }, { status: 500 });
   }
 }
